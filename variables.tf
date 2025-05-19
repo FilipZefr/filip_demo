dc0b24f789a87784564d77d3c86136e31d5dafe8
root <root@SKL-FILIP-VOJDA.seavus.biz>
2025-05-19 10:31:12 +0200

    Added backend config and VM resource

--- variables.tf
+++ variables.tf
@@ -1,22 +1,3 @@
-variable "client_id" {
-  description = "Service principal ID used for deployment"
-  type        = string
-}
-
-variable "client_secret" {
-  description = "Service principal secret"
-  type        = string
-}
-
-variable "tenant_id" {
-  description = "Azure tenant ID"
-  type        = string
-}
-
-variable "subscription_id" {
-  description = "Azure subscription ID"
-  type        = string
-}
 variable "location" {
   description = "Azure location"
   type        = string
